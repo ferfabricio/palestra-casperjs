8777d623e8091bb03ae698bbfd0ef770ec40c7ff
Fernando Fabricio dos Santos <ffsantos@uolinc.com>
2016-09-27 20:08:10 -0300

    some code style changes

--- js/script.js
+++ js/script.js
@@ -6,10 +6,15 @@ function getRandomInt(min, max) {
 
 function sendContactForm() {
   var delay = getRandomInt(1,10) *  1000;
+
   console.log(delay);
-  setTimeout(function(){
-    document.getElementById("messages").innerHTML = '<div class="alert alert-success" role="alert">Sua mensagem foi enviada com sucesso!</div>';
-  }, delay);
+
+  setTimeout(
+    function() {
+      document.getElementById("messages").innerHTML = '<div class="alert alert-success" role="alert">Sua mensagem foi enviada com sucesso!</div>';
+    },
+    delay
+  );
 }
 
 function resetContactForm() {
@@ -20,14 +25,20 @@ function comprarCamiseta() {
   var delay = getRandomInt(1,10) *  1000;
   console.log(delay);
   document.getElementById("venda").innerHTML = '<div class="spinner"><img src="img/spin.svg" class="rotate" /><p>Sua compra esta sendo processada!</p></div>';
-  setTimeout(function(){
-    document.getElementById("venda").innerHTML = '<div class="alert alert-success" role="alert">Compra realizada com sucesso! Aguarde nosso contato</div>';
-    refreshPage()
-  }, delay);
+  setTimeout(
+    function() {
+      document.getElementById("venda").innerHTML = '<div class="alert alert-success" role="alert">Compra realizada com sucesso! Aguarde nosso contato</div>';
+      refreshPage()
+    },
+    delay
+  );
 }
 
 function refreshPage() {
-    setTimeout(function(){
-        window.location.reload();
-    }, 10000);
+  setTimeout(
+    function() {
+      window.location.reload();
+    },
+    10000
+  );
 }

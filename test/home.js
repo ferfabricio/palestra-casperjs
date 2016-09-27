769032c29326ba9e41c204a0636863944dda3876
Fernando Fabricio dos Santos <ffsantos@uolinc.com>
2016-09-27 14:17:33 -0300

    changing descriptions

--- test/home.js
+++ test/home.js
@@ -1,11 +1,9 @@
-casper.start('http://localhost:8080/');
-
-casper.test.begin('Iniciando teste da home', 1, function(test){
-    casper.then(function(){
-        test.assertEquals('DevParaná', this.getTitle());
+casper.test.begin('Starting home tests', 1, function(test){
+    casper.start('http://localhost:8080/', function(){
+        test.assertEquals(this.getTitle(), 'DevParaná', 'verify page title');
     });
 
-    casper.run(function(){
+    casper.run(function() {
         test.done();
     });
 });
